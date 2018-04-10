61ec8aaa9458a4de5288c87ff7265394124ab122
Manu SG <manuelsg1996@gmail.com>
2018-04-10 12:28:16 +0200

    Palabras y botones
    Ya podemos enviar palabras y borrar al escribir

--- Pac0-1/src/main.cpp
+++ Pac0-1/src/main.cpp
@@ -11,19 +11,56 @@ char keys[rows][cols] = {
 };
 byte rowPins[rows] = {A2, A1, A0, 4}; //connect to the row pinouts of the keypad
 byte colPins[cols] = {A5, A4, A3, 7}; //connect to the column pinouts of the keypad
+
 Keypad keypad = Keypad( makeKeymap(keys), colPins, rowPins ,cols, rows );
+char men[20];
+int i = 0;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   char key = keypad.getKey();
+//palabr
 
-  if (key != NO_KEY){
-    Serial.println(key);
+if (key != NO_KEY) {
+  if (key != '*' && key != '#'){
+    men[i] = key;
+    i++;
+  }else if (key == '*'){
+    i = 0;
+    Serial.println(men);
+    memset(men, 0, sizeof(men));
+  }else if (key == '#'){
+    i = 0;
+    memset(men, 0, sizeof(men));
   }
+}
+/*  if (key != NO_KEY){
+    Serial.println(key);
+
+    if(key == 'A'){
+      analogWrite(11, 255);
+      analogWrite(10, 0);
+      analogWrite(9, 0);
+      }
+    if(key == 'B'){
+      analogWrite(11, 0);
+      analogWrite(10, 255);
+      analogWrite(9, 0);
+      }
+    if(key == 'C'){
+      analogWrite(11, 0);
+      analogWrite(10, 0);
+      analogWrite(9, 255);
+      }
+
+    if(key == 'D'){
+      analogWrite(11, 0);
+      analogWrite(10, 0);
+      analogWrite(9, 0);
+      }
+  }*/
 }
